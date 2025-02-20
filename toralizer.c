2ddedbfcecabc61300887973816e549d554d379d
Ryuk <126102004+raviwhitehat@users.noreply.github.com>
2025-02-21 00:22:14 +0530

    created toralizer.c

---
+++ toralizer.c
@@ -0,0 +1,50 @@
+#include "toralizer.h"
+
+
+int main(int argc,char *argsv[])
+{
+
+int Port;
+char* Ip;
+
+if(argc < 3){
+
+fprintf(stderr,"Usage : %s <host> <port>",argsv[0]);
+
+}
+
+
+Ip = argsv[1];
+Port = atoi(argsv[2]);
+
+int s =socket(AF_INET,SOCK_STREAM,0);
+
+if (s<0){
+perror("socket error");
+return -1;
+
+}
+
+
+struct sockaddr_in sock;
+
+
+
+
+sock.sin_family = AF_INET;
+sock.sin_port = htons(PROXYPORT);
+sock.sin_addr.s_addr = inet_addr(PROXY);
+
+if (connect(s,(struct sockaddr *)&sock,sizeof(sock))){
+
+perror("connection error");
+
+return -1;
+}
+
+printf("Connection Succesful.");
+close(s);
+
+return 0;
+}
+
